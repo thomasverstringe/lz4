e0bb121cb338adf8942672092ccb6eba50b27fa6
thomasverstringe <135336804+thomasverstringe@users.noreply.github.com>
2023-09-01 15:09:53 +0200

    Add declaration to user memory functions

--- lib/lz4.c
+++ lib/lz4.c
@@ -212,6 +212,9 @@
 /* memory management functions can be customized by user project.
  * Below functions must exist somewhere in the Project
  * and be available at link time */
+void* LZ4_user_malloc(size_t s);
+void* LZ4_user_calloc(size_t n, size_t s);
+void  LZ4_user_free(void* p);
 # define ALLOC(s)          LZ4_user_malloc(s)
 # define ALLOC_AND_ZERO(s) LZ4_user_calloc(1,s)
 # define FREEMEM(p)        LZ4_user_free(p)
